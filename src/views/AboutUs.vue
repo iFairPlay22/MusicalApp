71330c71fcccfa29fd6f2b0709d8f105cef7c99a
iFairPlay22 <44100687+iFairPlay22@users.noreply.github.com>
2019-07-17 11:41:05 +0200

    About Us

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,71 +1,79 @@
 <template>
-  <div id="app">
-    <v-app id="content">
-      <mast-head/>
-    
-      <v-layout>
-        <v-flex xs12 sm6 offset-sm3>
-          <v-card>
-            <v-img
-              class="white--text"
-              height="200px"
-              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-            >
-              <v-container fill-height fluid>
-                <v-layout fill-height>
-                  <v-flex xs12 align-end flexbox>
-                    <span class="headline">Top 10 Australian beaches</span>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-img>
-            <v-card-title>
-              <div>
-                <span class="grey--text">Number 10</span><br>
-                <span>Whitehaven Beach</span><br>
-                <span>Whitsunday Island, Whitsunday Islands</span>
-              </div>
-            </v-card-title>
-            <v-card-actions>
-              <v-btn flat color="orange">Share</v-btn>
-              <v-btn flat color="orange">Explore</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
-
-      <router-view/>
-
-      <mast-foot/>
-      </v-app>
-  </div>
+      <v-container grid-list-md>
+        <v-layout>
+          <v-flex 
+              xs4 
+              v-for="({name, title, bio, imageLink}, i) in cardContent"
+              :key="i">
+            <v-card>
+              <v-img
+                class="white--text"
+                height="200px"
+                :src="require(imageLink)"
+                alt="photo de Marc Denouel"
+              >
+                <v-container 
+                  fill-height 
+                  fluid 
+                  text-xs-center
+                >
+                  <v-layout 
+                    fill-height
+                  >
+                    <v-flex 
+                      xs12 
+                      flexbox
+                    >
+                      <h1 class="headline"> 
+                        {{ name }}
+                      </h1>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-img>
+              <v-card-title>
+                <div>
+                  <span class="grey--text"> 
+                    {{ title }}  
+                  </span><br>
+                  <span> 
+                    {{ bio }}
+                  </span>
+                </div>
+              </v-card-title>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
 </template>
 
 <script>
-  import MastHead from '../components/MastHead';
-  import MastFoot from '../components/MastFoot';
 
   export default {
     name: 'AboutUs',
     data () {
       return {
-        
+        cardContent: [
+          {
+            name: "Marc Denouel",
+            title: "Professeur de musique",
+            bio: "En tant que professeur de piano, j'ai créé une méthode pédagogique basée sur les intelligences multiples. Celle-ci porte ses fruits, mes élèves s'épanouissant musicalement sous la forme souhaitée.",
+            imageLink: "../assets/AboutUs/MarcDenouel.jpg"
+          },
+          {
+            name: "Ewen Bouquet",
+            title: "Etudiant en DUT informatique",
+            bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            imageLink: "../assets/AboutUs/EwenBouquet.jpg"
+          },
+          {
+            name: "Baptiste Devessier",
+            title: "Etudiant à l'école 42",
+            bio: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            imageLink: "../assets/AboutUs/BaptisteDevessier.jpg"
+          }
+        ]
       }
-    },
-    components: {
-      MastHead,
-      MastFoot
     }
   }
 </script>
-
-<style>
-    #content {
-    background: url('../assets/background.jpg');
-    height: 100vh;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    position: relative;
-    }
-</style>
