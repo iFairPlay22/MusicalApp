9548210d3b711a87b7169533278ce4759686a903
«iFairPlay22» <>
2019-08-11 11:42:18 +0200

    test migrations

---
+++ musical-app-back/Modules/Questionnary/Database/Migrations/2019_08_10_162612_propositions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PropositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('propositions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
